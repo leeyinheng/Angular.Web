fa101f8b4fbbd189591ed7867047c5ce90dfdbdb
Henry Lee <leeyinheng@yahoo.com>
2019-11-20 00:36:31 +0800

    logo update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   template: `
   <div class="container">
-  <span><img src='../assets/Photos/tealogo.jpg' width=20% height=20% /></span>
+ 
   <h1 class="lead"> {{title}}</h1>
 
   <router-outlet></router-outlet>
